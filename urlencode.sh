38bfdefd52ed29c4e4a756b3c3decacfb37537c5
Richard Kooijman <rkooijman@quicknet.nl>
2021-09-20 00:30:22 +0200

    added mounts added Docker binds

--- urlencode.sh
+++ urlencode.sh
@@ -10,11 +10,13 @@ function rawurlencode {
     for (( pos=0 ; pos<strlen ; pos++ )); do
         c=${string:${pos}:1}
         case "$c" in
-            _.~a-zA-Z0-9] ) o="${c}" ;;
-             )              printf -v o '%%%02x' "'$c"
+            [-_.~a-zA-Z0-9] ) o="${c}" ;;
+            * )               printf -v o '%%%02x' "'$c"
         esac
         encoded+="${o}"
     done
     echo "${encoded}"    # You can either set a return variable (FASTER)
     REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
 }
+
+rawurlencode $@
